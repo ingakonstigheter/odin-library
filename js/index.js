5de56e0cb68858bec6868bc013cb2f9682b55fa4
konstigheter <106750829+ingakonstigheter@users.noreply.github.com>
2025-05-20 13:05:10 +0200

    skeleton html css js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//   LIBRARY    //
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    
+}
